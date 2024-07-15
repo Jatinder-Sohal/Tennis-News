af8ecebdc8d8a09825025af48d3634d539694a26
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2024-07-15 18:39:12 +0100

    Setting up the springboot backend and react frontend on different ports while in the same directory.

---
+++ src/test/java/JS/Tennis_News/TennisNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package JS.Tennis_News;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
